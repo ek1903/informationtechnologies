188715319edece02b8b58e4597603f71ba32c392
elenekakhidze <elene-ka@mail.ru>
2016-10-26 12:41:39 +0300

    Update 1.cpp

--- dz2/1.cpp
+++ dz2/1.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
+double binpow (double a, int n) {
+	if (n < 0) return 1 / binpow (a, -n);
+	if (n == 0)
+		return 1;
+	if (n % 2 == 1)
+		return binpow (a, n-1) * a;
+	else {
+		int b = binpow (a, n/2);
+		return b * b;
+	}
+}
 int main() {
-    int a, b, h, i;
-    h=1;
-    cin >> a >> b;
-    for (i = 0; i < b; i++)
-        {
-        h = h * a;
-        }
-    cout << h << endl;
-    return 0;
+	int a, b;
+	cin >> a >> b;
+	cout << binpow(a, b);
+	return 0;
 }

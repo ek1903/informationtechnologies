110558ad83cbd9c2c7d1ac051c7a55208487509d
elenekakhidze <elene-ka@mail.ru>
2016-10-26 12:29:27 +0300

    Update dz1.cpp

--- dz1.cpp
+++ dz1.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int m, n, a;
+  int m, n, a, gcd;
     cin >> m >> n;
     a = m * n;
-    while (m != n) {
+    while (m != 0 && n !=0) {
         if (m > n) {
             m = m % n;
         } else {
             n = n % m;
         }
     }
-    cout << a / m;
+    gcd = max(m, n);
+    cout << a / gcd;
     return 0;
 }

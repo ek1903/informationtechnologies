4a1b465b91de8d9bf0e5325dd70262be0aab63c6
elenekakhidze <elene-ka@mail.ru>
2016-11-09 23:41:48 +0300

    Create 5.cpp

---
+++ dz3/5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, m, c = 1;
+    cin >> n >> m;
+    int** snake = new int*[n];
+    for(int i = 0; i < n; ++i) {
+          snake[i] = new int[m];
+    }
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < m; j++) {
+                snake[i][j] = c;
+                c++;
+            }
+        } else { 
+            for (int j = m - 1; j >= 0; j--) {
+                snake[i][j] = c;
+                c++;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << snake[i][j] << '\t' ;
+        }
+        cout << endl;
+    }      
+    return 0;
+}
